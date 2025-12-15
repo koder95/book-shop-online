b742b59b250884a1d6511368f1fe643f3b87d520
Kamil Jan Mularski <kammul@outlook.com>
2025-12-15 15:30:08 +0100

    Project initiation

---
+++ src/main/java/pl/koder95/bso/BookShopOnlineApplication.java
@@ -0,0 +1,13 @@
+package pl.koder95.bso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShopOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookShopOnlineApplication.class, args);
+    }
+
+}
