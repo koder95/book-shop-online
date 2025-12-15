4f523ecaf2e92d2e63965ba0d4c77cb4b81549a9
Kamil Jan Mularski <kammul@outlook.com>
2025-12-15 15:30:08 +0100

    Project initiation

---
+++ src/test/java/pl/koder95/bso/BookShopOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.koder95.bso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
