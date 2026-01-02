052f383decf3f34140db26a946bc72ed580ed548
Kamil Jan Mularski <kammul@outlook.com>
2026-01-02 14:54:27 +0100

    Apply changes from review
    Replaces @Component with @Repository in BookRepository for proper Spring Data semantics. Adds a default value of false for the is_deleted column in the books table changelog to ensure non-nullable constraint is satisfied.

--- src/main/java/pl/koder95/bso/repository/BookRepository.java
+++ src/main/java/pl/koder95/bso/repository/BookRepository.java
@@ -1,9 +1,9 @@
 package pl.koder95.bso.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import pl.koder95.bso.model.Book;
 
-@Component
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
