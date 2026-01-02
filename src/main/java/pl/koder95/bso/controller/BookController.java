c3d4675b6b19b5a13eccbebeb091cd3f1d00737d
Kamil Jan Mularski <kammul@outlook.com>
2026-01-02 17:45:43 +0100

    Refactor exceptions handling in BookController
    Moved exception handling from BookController to BookServiceImpl, replacing custom exceptions with ResponseStatusException for consistent HTTP error responses. This simplifies controller logic and centralizes error management in the service layer.

--- src/main/java/pl/koder95/bso/controller/BookController.java
+++ src/main/java/pl/koder95/bso/controller/BookController.java
@@ -1,10 +1,8 @@
 package pl.koder95.bso.controller;
 
-import jakarta.persistence.PersistenceException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 import pl.koder95.bso.dto.BookDto;
 import pl.koder95.bso.dto.CreateBookRequestDto;
-import pl.koder95.bso.exception.DataProcessingException;
-import pl.koder95.bso.exception.EntityNotFoundException;
 import pl.koder95.bso.service.BookService;
 
 @RestController
@@ -34,33 +29,17 @@ public class BookController {
 
     @GetMapping("/{id}")
     public BookDto get(@PathVariable Long id) {
-        try {
-            return bookService.get(id);
-        } catch (EntityNotFoundException e) {
-            throw new ResponseStatusException(HttpStatusCode.valueOf(404), e.getMessage());
-        }
+        return bookService.get(id);
     }
 
     @PostMapping
     public BookDto create(@RequestBody CreateBookRequestDto createRequest) {
-        try {
-            return bookService.save(createRequest);
-        } catch (DataProcessingException e) {
-            Throwable cause = e.getCause();
-            if (cause instanceof PersistenceException pe) {
-                throw new ResponseStatusException(HttpStatusCode.valueOf(409), pe.getMessage());
-            }
-            throw new ResponseStatusException(HttpStatusCode.valueOf(400));
-        }
+        return bookService.save(createRequest);
     }
 
     @PutMapping("/{id}")
     public BookDto update(@PathVariable Long id, @RequestBody CreateBookRequestDto book) {
-        try {
-            return bookService.update(id, book);
-        } catch (DataProcessingException e) {
-            throw new ResponseStatusException(HttpStatusCode.valueOf(400));
-        }
+        return bookService.update(id, book);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
