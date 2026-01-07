e68e46f9cf705f47d9c850ab6107c870e06bedf0
Kamil Jan Mularski <kammul@outlook.com>
2026-01-07 23:42:11 +0100

    Refactor controller binding of `search` method
    Simplified BookSearchParametersDto by removing @RequestParam annotations and constructor, using a compact constructor for default values. Updated BookController to bind search parameters with @ModelAttribute for improved request mapping.

--- src/main/java/pl/koder95/bso/controller/BookController.java
+++ src/main/java/pl/koder95/bso/controller/BookController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -51,7 +52,7 @@ public class BookController {
     }
 
     @GetMapping("/search")
-    public List<BookDto> search(BookSearchParametersDto params) {
+    public List<BookDto> search(@ModelAttribute BookSearchParametersDto params) {
         return bookService.search(params);
     }
 }
--- src/main/java/pl/koder95/bso/dto/BookSearchParametersDto.java
+++ src/main/java/pl/koder95/bso/dto/BookSearchParametersDto.java
@@ -2,7 +2,6 @@ package pl.koder95.bso.dto;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public record BookSearchParametersDto(String[] titles,
                                       String[] authors,
@@ -10,22 +9,13 @@ public record BookSearchParametersDto(String[] titles,
                                       BigDecimal priceMin,
                                       BigDecimal priceMax) {
 
-    public BookSearchParametersDto(
-            @RequestParam(required = false) String[] titles,
-            @RequestParam(required = false) String[] authors,
-            @RequestParam(required = false) String[] isbns,
-            @RequestParam(required = false, name = "price-min") BigDecimal priceMin,
-            @RequestParam(required = false, name = "price-max") BigDecimal priceMax
-    ) {
-        this.titles = titles == null
+    public BookSearchParametersDto {
+        titles = titles == null
                 ? new String[0] : Arrays.stream(titles).toArray(String[]::new);
-        this.authors = authors == null
+        authors = authors == null
                 ? new String[0] : Arrays.stream(authors).toArray(String[]::new);
-        this.isbns = isbns == null
+        isbns = isbns == null
                 ? new String[0] : Arrays.stream(isbns).toArray(String[]::new);
-        this.priceMin = priceMin;
-        this.priceMax = priceMax;
-        System.out.println("Created: " + this);
     }
 
     @Override
