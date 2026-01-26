024b185df81503e9722cc144f5b09f5247774259
Kamil Jan Mularski <kammul@outlook.com>
2026-01-26 23:03:49 +0100

    Add password encoding to user registration
    Introduced a PasswordEncoder component using SHA-512 and Base64 encoding. Updated user registration to encode passwords before saving to the repository, enhancing security.

--- src/main/java/pl/koder95/bso/service/impl/UserServiceImpl.java
+++ src/main/java/pl/koder95/bso/service/impl/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package pl.koder95.bso.service.impl;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import pl.koder95.bso.dto.RegisterUserRequestDto;
 import pl.koder95.bso.dto.UserDto;
@@ -16,6 +20,7 @@ import pl.koder95.bso.service.UserService;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDto register(RegisterUserRequestDto requestDto) throws RegistrationException {
@@ -24,7 +29,9 @@ public class UserServiceImpl implements UserService {
                     "Email %s already exists".formatted(requestDto.getEmail())
             );
         }
-        User saved = userRepository.save(userMapper.toModel(requestDto));
+        User model = userMapper.toModel(requestDto);
+        model.setPassword(passwordEncoder.encode(model.getPassword()));
+        User saved = userRepository.save(model);
         return userMapper.toDto(saved);
     }
 
@@ -48,5 +55,18 @@ public class UserServiceImpl implements UserService {
     @Override
     public Optional<UserDto> findByEmail(String email) {
         return userRepository.findByEmail(email).map(userMapper::toDto);
+    }
+
+    @Component
+    public static class PasswordEncoder {
+        public String encode(String password) {
+            MessageDigest md;
+            try {
+                md = MessageDigest.getInstance("sha-512");
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+            return Base64.getEncoder().encodeToString(md.digest(password.getBytes()));
+        }
     }
 }
