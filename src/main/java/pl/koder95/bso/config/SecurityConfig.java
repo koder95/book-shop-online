65a82160ed75da1f13bdd1e698c4b9ae1272473e
Kamil Jan Mularski <kammul@outlook.com>
2026-02-19 13:08:22 +0100

    Update SecurityConfig.java
    Co-Authored-By: Mateusz Lubiński <mateuszlubinski@hotmail.com>

--- src/main/java/pl/koder95/bso/config/SecurityConfig.java
+++ src/main/java/pl/koder95/bso/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -39,7 +38,6 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .userDetailsService(userDetailsService)
                 .build();
