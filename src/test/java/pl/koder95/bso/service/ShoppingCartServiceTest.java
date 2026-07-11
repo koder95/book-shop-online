58c880e9a93803a4c4a0654d594d0550a7e6a001
Kamil Jan Mularski <kontakt@koder95.pl>
2026-07-11 22:28:11 +0200

    Create ShoppingCartServiceTest.java

---
+++ src/test/java/pl/koder95/bso/service/ShoppingCartServiceTest.java
@@ -0,0 +1,92 @@
+package pl.koder95.bso.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import pl.koder95.bso.dto.ShoppingCartResponseDto;
+import pl.koder95.bso.factory.ShoppingCartFactory;
+import pl.koder95.bso.mapper.CartItemMapper;
+import pl.koder95.bso.mapper.ShoppingCartMapper;
+import pl.koder95.bso.model.Role;
+import pl.koder95.bso.model.RoleName;
+import pl.koder95.bso.model.ShoppingCart;
+import pl.koder95.bso.model.User;
+import pl.koder95.bso.repository.BookRepository;
+import pl.koder95.bso.repository.CartItemRepository;
+import pl.koder95.bso.repository.ShoppingCartRepository;
+import pl.koder95.bso.service.impl.ShoppingCartServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
+public class ShoppingCartServiceTest {
+    @Mock
+    private BookRepository bookRepository;
+    @Mock
+    private ShoppingCartRepository shoppingCartRepository;
+    @Mock
+    private CartItemRepository cartItemRepository;
+    @Mock
+    private CartItemMapper cartItemMapper;
+    @Mock
+    private ShoppingCartMapper shoppingCartMapper;
+    @Mock
+    private ShoppingCartFactory shoppingCartFactory;
+    @InjectMocks
+    private ShoppingCartServiceImpl shoppingCartService;
+
+    @Test
+    void getShoppingCart_withoutAuth_throwIllegalStateException() {
+        assertThrows(IllegalStateException.class, () -> shoppingCartService.getShoppingCart());
+    }
+
+    @Test
+    void getShoppingCart_withAuthNotCreatedYetAsUser_ok() {
+        Mockito.when(shoppingCartRepository.findById(1L)).thenReturn(java.util.Optional.empty());
+        Role userRole = new Role();
+        userRole.setName(RoleName.ROLE_USER);
+        User authenticated = new User();
+        authenticated.setId(1L);
+        authenticated.setRoles(Set.of(userRole));
+        authenticated.setEmail("user@example.com");
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                authenticated, null, authenticated.getAuthorities()
+        );
+        ShoppingCart cart = new ShoppingCart();
+        cart.setId(1L);
+        cart.setUser(authenticated);
+        cart.setCartItems(Set.of());
+        Mockito.when(shoppingCartFactory.createShoppingCart(authenticated)).thenReturn(cart);
+        ShoppingCartResponseDto expected = new ShoppingCartResponseDto(
+                cart.getId(), authenticated.getId(), List.of()
+        );
+        Mockito.when(shoppingCartRepository.save(cart)).thenReturn(cart);
+        Mockito.when(shoppingCartMapper.toResponseDto(Mockito.any())).thenReturn(expected);
+        ShoppingCartResponseDto actual;
+        try (var mockedStatic = Mockito.mockStatic(SecurityContextHolder.class)) {
+            SecurityContext mockedSecurityContext = Mockito.mock();
+            mockedStatic.when(SecurityContextHolder::getContext).thenReturn(mockedSecurityContext);
+            Mockito.when(mockedSecurityContext.getAuthentication()).thenReturn(auth);
+            actual = shoppingCartService.getShoppingCart();
+            Mockito.verify(mockedSecurityContext, Mockito.times(2)).getAuthentication();
+            mockedStatic.verify(SecurityContextHolder::getContext, Mockito.times(2));
+            Mockito.verifyNoMoreInteractions(mockedSecurityContext);
+            mockedStatic.verifyNoMoreInteractions();
+        }
+        assertEquals(expected, actual);
+        Mockito.verifyNoMoreInteractions(
+                shoppingCartRepository, shoppingCartFactory, shoppingCartMapper
+        );
+        Mockito.verifyNoInteractions(bookRepository, cartItemRepository, cartItemMapper);
+    }
+}
