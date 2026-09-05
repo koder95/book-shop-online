8581481e347767770b2262db096cf620bccb4d7a
Kamil Jan Mularski <kontakt@koder95.pl>
2026-09-05 18:34:57 +0200

    Rename ShoppingCartControllerTest

--- src/test/java/pl/koder95/bso/controller/CartControllerTest.java
+++ src/test/java/pl/koder95/bso/controller/CartControllerTest.java
@@ -1,5 +1,7 @@
 package pl.koder95.bso.controller;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -9,11 +11,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(MockitoExtension.class)
-public class ShoppingCartControllerTest {
+public class CartControllerTest {
     protected static MockMvc mockMvc;
 
     @BeforeAll
