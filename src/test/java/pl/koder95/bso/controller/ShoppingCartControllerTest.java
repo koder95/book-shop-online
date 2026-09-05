14c9d529dff98816a52fe9e13795ca4db3a7e38b
Kamil Jan Mularski <kontakt@koder95.pl>
2026-09-05 16:13:45 +0200

    Add ShoppingCartControllerTest

---
+++ src/test/java/pl/koder95/bso/controller/ShoppingCartControllerTest.java
@@ -0,0 +1,25 @@
+package pl.koder95.bso.controller;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(MockitoExtension.class)
+public class ShoppingCartControllerTest {
+    protected static MockMvc mockMvc;
+
+    @BeforeAll
+    static void setUp(@Autowired WebApplicationContext webApplicationContext) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+}
